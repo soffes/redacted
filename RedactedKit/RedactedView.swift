d927b9e799ee7bd66fd62060eb654c169eded7e3
Sam Soffes <sam@soff.es>
2015-05-01 16:15:07 -0700

    Update X

--- RedactedKit/RedactedView.swift
+++ RedactedKit/RedactedView.swift
@@ -6,15 +6,8 @@
 //  Copyright (c) 2015 Nothing Magical Inc. All rights reserved.
 //
 
-#if os(iOS)
-	import UIKit
-	public typealias View = UIView
-#else
-	import AppKit
-	public typealias View = NSView
-#endif
-
 import X
+import QuartzCore
 
 public class RedactedView: View {
 
@@ -45,64 +38,41 @@ public class RedactedView: View {
 
 	// MARK: - View
 
-	#if os(iOS)
-		public override func layoutSubviews() {
-			super.layoutSubviews()
-			layoutLayers()
-		}
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+		layoutLayers()
+	}
 
-		public override func didMoveToWindow() {
-			super.didMoveToWindow()
-			updateLayerScale()
-		}
-	#else
-		public override func layout() {
-			super.layout()
-			layoutLayers()
-		}
-
-		public override func viewDidMoveToWindow() {
-			super.viewDidMoveToWindow()
-			updateLayerScale()
-		}
-	#endif
+	public override func didMoveToWindow() {
+		super.didMoveToWindow()
+		updateLayerScale()
+	}
 
 
 	// MARK: - Private
 
-	private var backingLayer: CALayer! {
-		#if os(iOS)
-			return layer
-		#else
-			return layer
-		#endif
-	}
-
 	private func initialize() {
-		#if os(OSX)
-			wantsLayer = true
-		#endif
-
-		backingLayer.backgroundColor = Color(red: 0.863, green: 0.863, blue: 0.863, alpha: 1).CGColor
-		backingLayer.addSublayer(redactedLayer)
+		wantsLayer = true
+		let layer: CALayer? = self.layer
+		layer?.backgroundColor = Color(red: 0.863, green: 0.863, blue: 0.863, alpha: 1).CGColor
+		layer?.addSublayer(redactedLayer)
 		layoutLayers()
 	}
 
 	private func layoutLayers() {
+		let layer: CALayer? = self.layer
+
 		CATransaction.begin()
 		CATransaction.setDisableActions(true)
-		redactedLayer.frame = backingLayer.bounds
+		redactedLayer.frame = layer?.bounds ?? CGRectZero
 		CATransaction.commit()
 	}
 
 	private func updateLayerScale() {
-		#if os(iOS)
-			let scale = window?.screen.scale ?? 1.0
-		#else
-			let scale = window?.screen?.backingScaleFactor ?? 1.0
-		#endif
-
-		backingLayer.contentsScale = scale
+		let screen: Screen? = window?.screen
+		let layer: CALayer? = self.layer
+		let scale = screen?.scale ?? 1.0
+		layer?.contentsScale = scale
 		redactedLayer.contentsScale = scale
 	}
 }
