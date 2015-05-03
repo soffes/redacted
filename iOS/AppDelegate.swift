a65f1e6ac36ac2c19568a46dd284ba1fa38cbd11
Sam Soffes <sam@soff.es>
2015-05-03 15:15:24 -0400

    Ditch iOS storyboards

--- iOS/AppDelegate.swift
+++ iOS/AppDelegate.swift
@@ -9,9 +9,21 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder {
 
 	// MARK: - Properties
 	
-	var window: UIWindow?
+	lazy var window: UIWindow? = {
+		let window = UIWindow(frame: UIScreen.mainScreen().bounds)
+		window.rootViewController = UINavigationController(rootViewController: EditorViewController())
+		return window
+	}()
+}
+
+
+extension AppDelegate: UIApplicationDelegate {
+	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+		window?.makeKeyAndVisible()
+		return true
+	}
 }
