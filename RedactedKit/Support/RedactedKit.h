cd141eaeafabf041a24639abc574e4c0414358f9
Sam Soffes <sam@soff.es>
2016-09-13 22:56:04 -0500

    Organize

--- RedactedKit/Support/RedactedKit.h
+++ RedactedKit/Support/RedactedKit.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double RedactedKitVersionNumber;
 
 //! Project version string for RedactedKit.
 FOUNDATION_EXPORT const unsigned char RedactedKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <RedactedKit/PublicHeader.h>
-
-
