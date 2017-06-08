8fff7244d1648c4f627e91b486eb5c2fd9f27203
Sam Soffes <sam@soff.es>
2017-06-08 14:49:40 -0700

    Hook up gestures

--- Redacted-iOS/Sources/EditorViewController.swift
+++ Redacted-iOS/Sources/EditorViewController.swift
@@ -51,7 +51,24 @@ class EditorViewController: UIViewController {
 			redactedView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
 		])
 
+		// TODO: Remove
 		redactedView.originalImage = #imageLiteral(resourceName: "ScreenShot")
+
+		let pan = UIPanGestureRecognizer(target: self, action: #selector(panned))
+		view.addGestureRecognizer(pan)
+
+		let tap = UITapGestureRecognizer(target: self, action: #selector(tapped))
+		view.addGestureRecognizer(tap)
+
+		let twoFingerTap = UITapGestureRecognizer(target: self, action: #selector(twoFingerTapped))
+		twoFingerTap.numberOfTouchesRequired = 2
+		view.addGestureRecognizer(twoFingerTap)
+
+//		placeholderLabel.stringValue = string("DRAG_TO_GET_STARTED")
+//
+//		if !UserDefaults.standard.bool(forKey: "CreatedRedaction") {
+//			setupTutorial()
+//		}
 	}
 
 	override func viewWillAppear(_ animated: Bool) {
@@ -68,5 +85,36 @@ class EditorViewController: UIViewController {
 
 	override var prefersStatusBarHidden: Bool {
 		return true
+	}
+
+
+	// MARK: - Actions
+
+	//	func shareImage(fromView sender: NSView) {
+	//		if let image = renderedImage {
+	//			let sharingServicePicker = NSSharingServicePicker(items: [image])
+	//			sharingServicePicker.delegate = self
+	//			sharingServicePicker.show(relativeTo: CGRect.zero, of: sender, preferredEdge: .minY)
+	//		}
+	//	}
+
+	func panned(sender: UIPanGestureRecognizer) {
+		redactedView.drag(point: sender.location(in: view), state: sender.state)
+
+		//		if sender.state == .ended && redactedView.redactions.count > 0 {
+		//			hideTutorial()
+		//		}
+	}
+
+	func tapped(sender: UITapGestureRecognizer) {
+		if sender.state == .ended {
+			redactedView.tap(point: sender.location(in: view))
+		}
+	}
+
+	func twoFingerTapped(sender: UITapGestureRecognizer) {
+		if sender.state == .ended {
+			redactedView.tap(point: sender.location(in: view), exclusive: false)
+		}
 	}
 }
