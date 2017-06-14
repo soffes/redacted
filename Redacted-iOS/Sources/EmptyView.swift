8d94a17268aa8f588d7c11a6c62635869a3cdb26
Sam Soffes <sam@soff.es>
2017-06-14 16:45:05 -0400

    Update empty view color

--- Redacted-iOS/Sources/EmptyView.swift
+++ Redacted-iOS/Sources/EmptyView.swift
@@ -23,8 +23,8 @@ final class EmptyView: UIStackView {
 			titleEdgeInsets = UIEdgeInsets(top: 0, left: padding * 2, bottom: 0, right: 0)
 
 			titleLabel?.font = .systemFont(ofSize: 17, weight:  UIFontWeightMedium)
-			setTitleColor(UIColor(white: 1, alpha: 0.6), for: .normal)
-			setTitleColor(UIColor(white: 1, alpha: 0.5), for: .highlighted)
+			setTitleColor(UIColor(white: 1, alpha: 0.8), for: .normal)
+			setTitleColor(UIColor(white: 1, alpha: 0.7), for: .highlighted)
 
 			adjustsImageWhenHighlighted = false
 
@@ -49,7 +49,7 @@ final class EmptyView: UIStackView {
 		}
 
 		private func updateColors() {
-			let color = isHighlighted ? UIColor(white: 1, alpha: 0.5) : UIColor(white: 1, alpha: 0.6)
+			let color = titleColor(for: isHighlighted ? .highlighted : .normal)
 			imageView?.tintColor = color
 		}
 	}
