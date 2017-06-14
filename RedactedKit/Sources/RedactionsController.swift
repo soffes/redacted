0f4bbd9df2013bfac89291f75a32c7f0d633309c
Sam Soffes <sam@soff.es>
2017-06-14 16:20:47 -0400

    Import images at orientations correctly

--- RedactedKit/Sources/RedactionsController.swift
+++ RedactedKit/Sources/RedactionsController.swift
@@ -26,8 +26,39 @@ public final class RedactionsController {
 				#if os(OSX)
 					ciImage = CIImage(cgImage: image.cgImage!)
 				#else
-					let img = CIImage(cgImage: image.cgImage!)
-					ciImage = img.applying(CGAffineTransform(scaleX: 1, y: -1).concatenating(CGAffineTransform(translationX: 0, y: img.extent.height)))
+					var img = CIImage(cgImage: image.cgImage!)
+					img = img.applying(CGAffineTransform(scaleX: 1, y: -1).concatenating(CGAffineTransform(translationX: 0, y: img.extent.height)))
+
+					switch image.imageOrientation {
+					case .up:
+						break
+
+					case .down:
+						img = img.applying(CGAffineTransform(rotationAngle: .pi))
+
+					case .left:
+						img = img.applying(CGAffineTransform(rotationAngle: .pi / -2))
+
+					case .right:
+						img = img.applying(CGAffineTransform(rotationAngle: .pi / 2))
+
+					case .upMirrored:
+						img = img.applying(CGAffineTransform(scaleX: -1, y: 1).concatenating(CGAffineTransform(translationX: img.extent.width, y: 0)))
+
+					case .downMirrored:
+						img = img.applying(CGAffineTransform(rotationAngle: .pi))
+						img = img.applying(CGAffineTransform(scaleX: -1, y: 1).concatenating(CGAffineTransform(translationX: img.extent.width, y: 0)))
+
+					case .leftMirrored:
+						img = img.applying(CGAffineTransform(rotationAngle: .pi / -2))
+						img = img.applying(CGAffineTransform(scaleX: -1, y: 1).concatenating(CGAffineTransform(translationX: img.extent.width, y: 0)))
+						
+					case .rightMirrored:
+						img = img.applying(CGAffineTransform(rotationAngle: .pi / 2))
+						img = img.applying(CGAffineTransform(scaleX: -1, y: 1).concatenating(CGAffineTransform(translationX: img.extent.width, y: 0)))
+					}
+
+					ciImage = img
 				#endif
 			} else {
 				ciImage = nil
