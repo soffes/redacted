7dd778b9cedfb447c829865d1f577f8bf3933425
Sam Soffes <sam@soff.es>
2019-09-28 09:59:58 -0700

    Xcode 11.1

--- Modules/RedactedKit/Sources/RedactedKit.swift
+++ Modules/RedactedKit/Sources/RedactedKit.swift
@@ -2,12 +2,14 @@ import Foundation
 import X
 
 func bundle() -> Bundle? {
-	return Bundle(for: RedactedView.self)
+	let path = Bundle.main.path(forResource: "RedactedKitResources", ofType: "bundle")
+	return path.flatMap { Bundle(path: $0) }
 }
 
 public func string(_ key: String) -> String {
 	if let bundle = bundle() {
 		return bundle.localizedString(forKey: key, value: nil, table: nil)
 	}
+
 	return key
 }
