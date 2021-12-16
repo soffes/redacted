abbff3a8eb718b59465bc9a788b8f07d37469cdf
Sam Soffes <sam@soff.es>
2021-12-16 10:55:57 -0800

    Remove some unused stuff

--- Modules/RedactedKit/Sources/CoreGraphics+RedactedKit.swift
+++ Modules/RedactedKit/Sources/CoreGraphics+RedactedKit.swift
@@ -13,28 +13,6 @@ extension CGRect {
 			height: size.height
 		)
 	}
-
-	func aspectFit(_ aspectRatio: CGSize) -> CGRect {
-		let size = self.size.aspectFit(aspectRatio)
-		var origin = self.origin
-		origin.x += (self.size.width - size.width) / 2.0
-		origin.y += (self.size.height - size.height) / 2.0
-		return CGRect(origin: origin, size: size)
-	}
-}
-
-extension CGSize {
-	func aspectFit(_ aspectRatio: CGSize) -> CGSize {
-		let widthRatio = (width / aspectRatio.width)
-		let heightRatio = (height / aspectRatio.height)
-		var size = self
-		if widthRatio < heightRatio {
-			size.height = width / aspectRatio.width * aspectRatio.height
-		} else if heightRatio < widthRatio {
-			size.width = height / aspectRatio.height * aspectRatio.width
-		}
-		return CGSize(width: ceil(size.width), height: ceil(size.height))
-	}
 }
 
 extension CGPoint {
